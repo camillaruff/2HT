ee09ee032e383e8ab8c76b224ed81ab99d28749a
Camilla Rufino Pereira <134282829+camillaruff@users.noreply.github.com>
2023-05-22 09:28:36 -0300

    Add files via upload

---
+++ Lista 1 - quest4.py
@@ -0,0 +1,11 @@
+#Lista 1 - quest4
+
+
+#SOLICITAR INFO
+numero = int(input("Digite um número inteiro: "))
+
+#VERIFICACAO
+if numero % 2 == 0:
+    print("O número", numero, "é par.")
+else:
+    print("O número", numero, "é ímpar.")
