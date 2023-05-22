5af9b84b62aec6af0f562c2dd6b548391f60ff35
Camilla Rufino Pereira <134282829+camillaruff@users.noreply.github.com>
2023-05-22 09:49:34 -0300

    Add files via upload

---
+++ Lista 1 - quest9.py
@@ -0,0 +1,13 @@
+#Lista 1 - quest9
+
+
+#MEDIDAS DA PISCINA INFO
+largura = int(input("Escreva a largura da piscina (m): "))
+comprimento = int(input("Escreva o comprimento da piscina (m)": ))
+profundidade = int(input("Escreva a profundidade da piscina (m): "))
+
+#CALCULAR O VOLUME
+volume = largura * comprimento * profundidade
+
+#EXIBICAO
+print("O volume de água da piscina é:", volume, "metros cúbicos.")
