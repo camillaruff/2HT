6f72bb8524cb1c1390ca49f5e6b98be6c5e0c282
Camilla Rufino Pereira <134282829+camillaruff@users.noreply.github.com>
2023-05-22 09:25:29 -0300

    Add files via upload

---
+++ Lista 1 - quest3.py
@@ -0,0 +1,13 @@
+#Lista 1 - quest3
+
+
+#SOLICITAR INFO
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+numero3 = int(input("Digite o terceiro número: "))
+
+#MENOR NUM
+menor_numero = min(numero1, numero2, numero3)#função min()
+
+#EXIBICAO 
+print("O menor número é:", menor_numero)
